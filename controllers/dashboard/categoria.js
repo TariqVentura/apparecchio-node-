d0a4333afb4803bb52827d5f4b0d5cd7a64a3ba5
TariqVentura <20190052@ricaldone.edu.sv>
2023-02-20 19:41:36 -0600

    cambios 20 febrero 2023

---
+++ controllers/dashboard/categoria.js
@@ -0,0 +1,59 @@
+function openCreate() {
+    Swal.fire({
+        title: "Agregar Categoria",
+        input: "text",
+        inputPlaceholder: "Categoria",
+        showCancelButton: true,
+        confirmButtonColor: "#9e7676"
+    })
+}
+
+function openUpdate() {
+    Swal.fire({
+        title: "Actualizar Categoria",
+        input: "text",
+        inputPlaceholder: "Categoria",
+        showCancelButton: true,
+        confirmButtonColor: "#9e7676"
+    })
+}
+
+
+function openDelete() {
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+    })
+
+    swalWithBootstrapButtons.fire({
+        title: 'Estas seguro de eliminar esta categoria?',
+        text: "Sera eliminado para siempre!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Si eliminar!',
+        cancelButtonText: 'Cancelar',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            swalWithBootstrapButtons.fire(
+                'Elimnado!',
+                'La categoria a sido eliminada',
+                'success'
+            )
+        } else if (
+            /* Read more about handling dismissals below */
+            result.dismiss === Swal.DismissReason.cancel
+        ) {
+            swalWithBootstrapButtons.fire(
+                'Cancelada',
+                'La categoria no se a eliminado',
+                'error'
+            )
+        }
+    })
+}
+
+
---
+++ controllers/dashboard/marca.js
@@ -0,0 +1,59 @@
+function openCreate() {
+    Swal.fire({
+        title: "Agregar Marca",
+        input: "text",
+        inputPlaceholder: "Marca",
+        showCancelButton: true,
+        confirmButtonColor: "#9e7676"
+    })
+}
+
+function openUpdate() {
+    Swal.fire({
+        title: "Actualizar Marca",
+        input: "text",
+        inputPlaceholder: "Marca",
+        showCancelButton: true,
+        confirmButtonColor: "#9e7676"
+    })
+}
+
+
+function openDelete() {
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+    })
+
+    swalWithBootstrapButtons.fire({
+        title: 'Estas seguro de eliminar la marca?',
+        text: "Sera eliminado para siempre!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Si eliminar!',
+        cancelButtonText: 'Cancelar',
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            swalWithBootstrapButtons.fire(
+                'Elimnado!',
+                'La marca a sido eliminada',
+                'success'
+            )
+        } else if (
+            /* Read more about handling dismissals below */
+            result.dismiss === Swal.DismissReason.cancel
+        ) {
+            swalWithBootstrapButtons.fire(
+                'Cancelada',
+                'La marca no se a eliminado',
+                'error'
+            )
+        }
+    })
+}
+
+
