f013208e16924b967eb26179422893880459a961
Duko <20210089@ricaldone.edu.sv>
2023-04-13 14:58:00 -0600

    se encripto las contraseñas

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const md5 = require('md5')
+const bcrypt = require('bcrypt')
 const services = require('../services/dashboard/render')
 var users = require('../models/users')
 
@@ -9,43 +10,56 @@ exports.createUser = (req, res) => {
         return
     }
 
-    //crear usuario
-    const newUser = new users({
-        name: req.body.name,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        identity_card: req.body.identity_card,
-        user: req.body.user,
-        password: md5(req.body.password),
-        status: req.body.status
+    //encriptar contraseña
+    const saltRounds = 10
+    const EncryptedPassword = req.body.password
+
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+        bcrypt.hash(EncryptedPassword, salt, function (err, hash) {
+            //crear usuario
+            const newUser = new users({
+                name: req.body.name,
+                lastname: req.body.lastname,
+                email: req.body.email,
+                identity_card: req.body.identity_card,
+                user: req.body.user,
+                password: hash,
+                status: req.body.status
+            })
+
+            //guardar datos en la base
+            newUser
+                .save(newUser)
+                .then(data => {
+                    res.send(data)
+                })
+                .catch(err => {
+                    res.status(500).send({
+                        message: err.message || "Ocurrio un error al intentar ingresar el usuario"
+                    })
+                })
+        })
     })
 
-    //guardar datos en la base
-    newUser
-        .save(newUser)
-        .then(data => {
-            res.send(data)
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err.message || "Ocurrio un error al intentar ingresar el usuario"
-            })
-        })
+
+
 }
 
 exports.findOneUser = (req, res) => {
     const username = req.body.user
-    users.findOne({user:username})
+    users.findOne({ user: username })
         .then(user => {
             if (!user) {
-                
-            }else{
-                if (user.password != md5(req.body.password)) {
-                    res.status(400).send({ message: 'contraseña incorrecta' })
-                }else{
-                    res.redirect('/')
-                }               
-            }      
+                res.status(500).send({ message: "Usuario inexistente" })
+            } else {
+                bcrypt.compare(req.body.password, user.password, function(err, result) {
+                    if (result) {
+                        res.redirect('/')
+                    } else {
+                        res.status(500).send({ message: "Contraseña incorrecta" })
+                    }
+                })
+            }
         })
         .catch(err => {
             res.status(500).send({ message: err.message || "Ocurrio un error al tratar de obtener la informacion" })
