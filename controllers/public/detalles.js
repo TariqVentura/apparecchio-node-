1d7deb597033c1e422cacc4ff6b614291c30494b
Angeltribilin <angell.1906pinher@gmail.com>
2023-07-26 08:13:34 -0600

    factura sin template

--- controllers/public/detalles.js
+++ controllers/public/detalles.js
@@ -1,10 +1,2 @@
-function confirmacion() {
-    Swal.fire({
-        icon: 'success',
-        text: 'Compra realizada exitosamente',
-        footer: '<a href="/productos">Seguir comprando?</a>',
-        background: '#fff8eb',
-        color: '#584342',
-        button: '#fff8eb'
-    })
+function show() {
 }
